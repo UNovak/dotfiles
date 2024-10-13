469aee7b830929b7ce78aac0a6787130964217d6
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-13 17:52:29 +0200

    adjust yabai for use with stackline

--- .config/skhd/scripts/layout_toggle.zsh
+++ .config/skhd/scripts/layout_toggle.zsh
@@ -4,7 +4,9 @@ current=$(yabai -m query --spaces --space | jq -r .type)
 if [[ "$current" == "bsp" ]]; then
   new="stack"
   yabai -m space --layout "$new"
+  yabai -m config left_padding 28
 else
   new="bsp"
   yabai -m space --layout "$new"
+  yabai -m config left_padding 8
 fi
