1fd9839d31bb1be3ab5e7bc3eabf13faea37a5cb
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-22 02:17:51 +0200

    fix yabai padding issues

--- .config/skhd/scripts/layout_toggle.zsh
+++ .config/skhd/scripts/layout_toggle.zsh
@@ -1,13 +1,20 @@
 #!/bin/zsh
-current=$(yabai -m query --spaces --space | jq -r .type)
+current_layout=$(yabai -m query --spaces --space | jq -r .type)
+space_index=$(yabai -m query --spaces --space | jq -r .index)
 window_count=$(yabai -m query --windows --space | jq length)
 
-if [[ "$current" == "bsp" && $window_count -ge 2 ]]; then
+if [[ "$current_layout" == "bsp" && $window_count -ge 2 ]]; then
   new="stack"
   yabai -m space --layout "$new"
-  yabai -m config left_padding 28
-elif [[ "$current" == "stack" ]]; then
+  yabai -m config --space $space_index top_padding 32
+  yabai -m config --space $space_index right_padding 32
+  yabai -m config --space $space_index bottom_padding 32
+  yabai -m config --space $space_index left_padding 32
+elif [[ "$current_layout" == "stack" ]]; then
   new="bsp"
   yabai -m space --layout "$new"
-  yabai -m config left_padding 8
+  yabai -m config --space $space_index top_padding 16
+  yabai -m config --space $space_index right_padding 16
+  yabai -m config --space $space_index bottom_padding 16
+  yabai -m config --space $space_index left_padding 16
 fi
--- .config/yabai/layout.zsh
+++ .config/yabai/layout.zsh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 window_count=$(yabai -m query --windows --space | jq length)
-current=$(yabai -m query --spaces --space | jq -r .type)
+space_index=$(yabai -m query --spaces --space | jq -r .index)
+current_layout=$(yabai -m query --spaces --space | jq -r .type)
 
 # If there is only 1 window and the layout is 'stack', switch to 'bsp'
-if [[ "$window_count" -eq 1 && "$current" == "stack" ]]; then
+if [[ "$window_count" -eq 1 && "$current_layout" == "stack" ]]; then
   yabai -m space --layout bsp
-  yabai -m config left_padding 8
+  yabai -m config --space $space_index top_padding 32
+  yabai -m config --space $space_index right_padding 32
+  yabai -m config --space $space_index bottom_padding 32
+  yabai -m config --space $space_index left_padding 32
 fi
