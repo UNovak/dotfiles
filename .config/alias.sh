6efb3db1f613feb3f4c77178f10032ffa1f09960
UNovak <88665587+UNovak@users.noreply.github.com>
2025-05-23 16:44:39 +0200

    🔧 (zsh): update shell aliases

--- .config/alias.sh
+++ .config/alias.sh
@@ -7,11 +7,20 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 
 # git
-alias gs="git status"
-alias glg="git lg"
+alias g="git"
+alias ga="git add"
+alias gap="git add -p"
+
+alias gs="git status -s"
+alias gc="git commit --verbose"
+
+alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
+alias ds="git diff --cached --output-indicator-new=' ' --output-indicator-old=' '"
+
+alias gl="git log --color --decorate --abbrev-commit --date=human --pretty=format:'%C(yellow)%d%C(reset) %C(red)%h%C(reset) %C(white)%s%C(reset) %C(blue)<%an>%C(reset) %C(cyan)(%cr)%C(reset)'"
 
 # eza
-alias ezac='eza --no-filesize --no-symlinks --ignore-glob=".DS_Store|Icon'$'\r"'
+alias ezac='eza --no-filesize --ignore-glob="node_modules|.DS_Store|Icon'$'\r"'
 alias ls="ezac -F --time-style='+%Y-%m-%d %H:%M' --time=modified --smart-group --group-directories-last -b"
 alias lf="ezac --only-files"
 alias ld="ezac --only-dirs"
@@ -28,3 +37,12 @@ alias c="clear"
 
 # managing config files
 alias conf="/opt/homebrew/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# Expanding XDG_ env variables
+cache() {
+  print -r "${XDG_CACHE_HOME:-~/.cache}"
+}
+
+config() {
+  print -r "${XDG_CONFIG_HOME:-~/.config}"
+}
